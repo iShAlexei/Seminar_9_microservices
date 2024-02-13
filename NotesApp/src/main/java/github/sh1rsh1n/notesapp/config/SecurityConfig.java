48db18ae471adcfcf0243846ac99e38e094fbbe2
dev_hp_laptop <a.s.shirshin@mail.ru>
2024-02-13 22:14:27 +0300

    seminar9 commit #004_1

---
+++ NotesApp/src/main/java/github/sh1rsh1n/notesapp/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package github.sh1rsh1n.notesapp.config;
+
+//@EnableWebSecurity
+public class SecurityConfig {
+
+    //TODO
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/api/notes/**")
+//                        .authenticated())
+//                .oauth2ResourceServer(Customizer.withDefaults());
+//                        .authenticated());
+//        http.oauth2ResourceServer(Customizer.withDefaults());
+//        return http.build();
+//    }
+}
